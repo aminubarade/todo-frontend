de9905bc917b777e7a8c5262bd73c4ee8b453c65
aminubarade <aminubaradepage@gmail.com>
2023-04-20 14:35:04 +0100

    added user input fields

--- src/components/UserComponent/createUser.js
+++ src/components/UserComponent/createUser.js
@@ -1,12 +1,19 @@
 //import logo from './logo.svg';
 //import './App.css';
-import {Routes, Route} from 'react-router-dom';
-import Navbar from "../../components/navbar";
-import AppRoute from '../../Routes';
+// import {Routes, Route} from 'react-router-dom';
+// import Navbar from "../../components/navbar";
+// import AppRoute from '../../Routes';
 
 function CreateUser() {
   return (
     <div>
+       <form>
+          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Firstname" />
+          <input type="text" placeholder="Lastname" />
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Password" />
+       </form>
 
     </div>
   );
